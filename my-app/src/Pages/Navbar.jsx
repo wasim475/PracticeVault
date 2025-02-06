809ecb9c1ed90e1ee2ecc928bcc1993e3a6633ed
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-06 19:58:08 +0600

    create my-app for more practice react

---
+++ my-app/src/Pages/Navbar.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { IoMdMenu } from "react-icons/io";
+
+
+
+const Navbar = () => {
+    const [openMenu, setOpenMenu]= useState(false)
+  return (
+    <div>
+        <IoMdMenu onClick={()=>setOpenMenu(!openMenu)} className='text-2xl' />
+      <ul className={`bg-cyan-400 inline-block p-4 ${openMenu ? "duration-1000":"mt-60 duration-1000"}`} >
+        <li>Home</li>
+        <li>About</li>
+        <li>Blogs</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
